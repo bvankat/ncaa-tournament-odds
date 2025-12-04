11cb0b03e7d27dc8029f5e4cc4883a9ce855797b
Benjamin Vankat <bvankat@gmail.com>
2025-12-04 12:50:35 -0600

    Favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export function Header({ onHome, teams = [], onTeamSelect, onOpenPalette }: Head
     <>
       <nav className="bg-black text-white py-3 px-6 flex items-center justify-between">
         <button onClick={onHome} className="flex items-center gap-2 hover:opacity-80 transition-opacity">
-          <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+          <svg id="gauge" className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
             <path d="M12 13m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
             <path d="M13.45 11.55l2.05 -2.05"></path>
             <path d="M6.4 20a9 9 0 1 1 11.2 0z"></path>
