094d1aee63934c1eaede40ed9552203f20dc875d
Benjamin Vankat <bvankat@gmail.com>
2026-01-10 02:17:30 -0600

    Fixed formatRelativeTime string error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function formatRelativeTime(dateString: string) {
+export function formatRelativeTime(dateString: string | number) {
   if (!dateString) return '';
 
   const date = new Date(dateString);
